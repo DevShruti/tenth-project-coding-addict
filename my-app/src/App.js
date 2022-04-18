1d9a76f683d9f824569df492db87e05ca75f29b3
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2022-04-18 18:17:38 -0500

    button and form

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,8 +8,28 @@ function App() {
   const [isEditing, setIsEditing] = useState(false)
   const [editID, setEditID] = useState(null)
   const [alert, setAlert] = useState({show:false, msg:'', type:''})
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
   return (
-    <section className="section-center"></section>>
+    <section className="section-center">
+      <form className="grocery-form" onSubmit={handleSubmit}>
+        {alert.show && <Alert/>}
+        <h3>Grocery Bud</h3>
+        <div className ="form-control">
+          <input type="text"/>
+          <button type="submit" className ="submit-btn">
+            {isEditing ? 'edit' : 'submit'}
+          </button>
+
+        </div>
+      </form>
+      <div className ="grocery-container">
+        <List/>
+        <button className="clear-btn">clear items</button>
+      </div>
+    </section>
+
   )
 }
 
